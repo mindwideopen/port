56cfa3f99b9ca5c7b6e55fb7107efccbb28678df
Nikolay Litvinov <nlit3926@gmail.com>
2024-03-22 16:11:40 +0200

    solving svg social items size problem

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import {Skills} from "../src/layout/sections/Skills/Skills";
 import {Works} from "./layout/sections/works/Works";
 import {Testimonials} from "./layout/sections/testimonials/Testimonials";
 import {Contact} from "./layout/sections/contact/Contact";
+import {Footer} from "./layout/footer/Footer";
 
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
             <Works/>
             <Testimonials/>
             <Contact/>
+            <Footer/>
         </div>
 
 
---
+++ src/layout/footer/Footer.tsx
@@ -0,0 +1,80 @@
+import React from 'react';
+import styled from "styled-components";
+
+import {Icon} from "../../components/icon/Icon";
+import {SectionTitle} from "../../components/sectionTitle/SectionTitle";
+
+export const Footer = () => {
+    return (
+        <FooterStyled>
+
+
+            <Name>Nikolay</Name>
+
+            <SocialIconList>
+
+                <SocialItem>
+                    <SocialIconLink>
+                        <Icon width={'21px'} height={'21px'} viewBox={'0 0 21 21'} iconId={'instagram'}/>
+                    </SocialIconLink>
+                </SocialItem>
+
+                <SocialItem>
+                    <SocialIconLink>
+                        <Icon  iconId={'instagram'}/>
+                    </SocialIconLink>
+                </SocialItem>
+
+                <SocialItem>
+                    <SocialIconLink>
+                        <Icon   iconId={'instagram'}/>
+                    </SocialIconLink>
+                </SocialItem>
+
+                <SocialItem>
+                    <SocialIconLink>
+                        <Icon  iconId={'instagram'}/>
+                    </SocialIconLink>
+                </SocialItem>
+
+            </SocialIconList>
+            <Copyright>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, totam!</Copyright>
+
+        </FooterStyled>
+    );
+};
+
+const FooterStyled = styled.section`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+
+`
+
+const Name = styled.span`
+
+
+`
+
+const SocialIconList = styled.ul`
+    list-style: none;
+    display: flex;
+
+
+`
+
+const SocialIconLink = styled.ul`
+
+
+`
+const SocialItem = styled.li`
+
+
+`
+
+const Copyright = styled.small`
+
+
+`
+
