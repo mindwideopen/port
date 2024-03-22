fa87b52b64a9e62a9bfb524f4410e30185693a3b
Nikolay Litvinov <nlit3926@gmail.com>
2024-03-22 17:04:03 +0200

    solved svg social items size problem  + markeu up Footer Section

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -21,19 +21,19 @@ export const Footer = () => {
 
                 <SocialItem>
                     <SocialIconLink>
-                        <Icon  iconId={'instagram'}/>
+                        <Icon width={'21px'} height={'21px'} viewBox={'0 0 21 21'} iconId={'vk'}/>
                     </SocialIconLink>
                 </SocialItem>
 
                 <SocialItem>
                     <SocialIconLink>
-                        <Icon   iconId={'instagram'}/>
+                        <Icon width={'21px'} height={'21px'} viewBox={'0 0 21 21'} iconId={'linkedIn'}/>
                     </SocialIconLink>
                 </SocialItem>
 
                 <SocialItem>
                     <SocialIconLink>
-                        <Icon  iconId={'instagram'}/>
+                        <Icon width={'21px'} height={'21px'} viewBox={'0 0 21 21'} iconId={'telegram'}/>
                     </SocialIconLink>
                 </SocialItem>
 
@@ -60,6 +60,7 @@ const Name = styled.span`
 const SocialIconList = styled.ul`
     list-style: none;
     display: flex;
+    padding: 0;
 
 
 `
