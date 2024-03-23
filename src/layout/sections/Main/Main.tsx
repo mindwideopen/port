61fac985382b3e18eb379ac70210fc794543f084
Nikolay Litvinov <nlit3926@gmail.com>
2024-03-23 23:46:54 +0200

    Revert "doing Main Section"
    This reverts commit b688a5d0a4653fbb3df64746202c4fe38d0fdd66.

--- src/layout/sections/Main/Main.tsx
+++ src/layout/sections/Main/Main.tsx
@@ -2,27 +2,23 @@ import React from 'react';
 import styled from "styled-components";
 import photo from '../../../assets/images/photo1704753319.jpeg'
 import {FlexWrapper} from "../../../components/FlexWrapper";
-import {Container} from "../../../components/Container/Container";
 
 
 
 export const Main = () => {
     return (
         <MainStyled>
-            <Container>
-                <FlexWrapper justify={'space-between'} align={'center'} >
+            <FlexWrapper justify={'space-around'} align={'center'} >
 
-                    <div>
-                        <Greet>Hi There</Greet>
-                        <Name>I am Nikolay Litvinov</Name>
-                        <h1>A Web Developer.</h1>
-                    </div>
+                <div>
+                    <span>Hi There</span>
+                    <h2>I am Nikolay Litvinov</h2>
+                    <h1>A Web Developer.</h1>
+                </div>
 
-                    <Photo src={photo} alt="Фотка"/>
+                <Photo src={photo} alt="Фотка"/>
 
-                </FlexWrapper>
-        </Container>
-
+            </FlexWrapper>
         </MainStyled>
 
 
@@ -39,12 +35,7 @@ object-fit: cover
 `
 
  const MainStyled = styled.section`
-     display: flex;
-    
+    background-color: burlywood;
     height: 100vh;
 `
 
-const Greet = styled.span`
-`
-const Name = styled.span`
-`
