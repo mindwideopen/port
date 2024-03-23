123f4fd7a53a93ded1d1f4b8f09d37f5690ee381
Nikolay Litvinov <nlit3926@gmail.com>
2024-03-23 21:25:25 +0200

    added effect to header items

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import styled from "styled-components";
 import {Logo} from "../../components/logo/Logo";
-import {Menu} from "../../components/menu/Menu";
 import {Container} from "../../components/Container/Container";
 import {FlexWrapper} from "../../components/FlexWrapper";
 import {HeaderMenu} from "./headerMenu/HeaderMenu";
