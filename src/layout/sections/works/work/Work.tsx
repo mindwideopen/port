5a51d9238ecb93b133f0a0c2b9e34739269bf321
Nikolay Litvinov <nlit3926@gmail.com>
2024-04-09 21:41:10 +0300

    pixelPerfect.works: done (fixed position of button during hover)

--- src/layout/sections/works/work/Work.tsx
+++ src/layout/sections/works/work/Work.tsx
@@ -86,7 +86,7 @@ const ImageWrapper = styled.div`
         position: absolute;
         left: 50%;
         top: 50%;
-        transform: translate(-50%, -50%);
+        transform: translate(-50%, -23%);
         opacity: 0;
     }
     
