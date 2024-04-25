8ff9e4a8b8e28503ee826834dd7889a827b577ac
Nikolay Litvinov <nlit3926@gmail.com>
2024-04-25 18:56:40 +0300

    refactored Footer

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -1,123 +1,52 @@
 import React from 'react';
-import styled from "styled-components";
+import {S} from './FooterStyles'
 import {Icon} from "../../components/icon/Icon";
-import {theme} from "../../styles/Theme";
-import {font} from "../../styles/Common";
 
 
-export const Footer = () => {
+
+const socialItemData = [
+    {
+        iconId:'instagram'
+    },
+    {
+        iconId:'vk'
+    },
+    {
+        iconId:'linkedIn'
+    },
+    {
+        iconId:'telegram'
+    }
+
+]
+
+export const Footer: React.FC = () => {
     return (
-        <FooterStyled>
+        <S.FooterStyled>
 
 
-            <Name>Nikolay</Name>
+            <S.Name>Nikolay</S.Name>
 
-            <SocialIconList>
 
-                <SocialItems>
-                    <SocialIconLink>
-                        <Icon width={'21px'} height={'21px'} viewBox={'0 0 21 21'} iconId={'instagram'}/>
-                    </SocialIconLink>
-                </SocialItems>
 
-                <SocialItems>
-                    <SocialIconLink>
-                        <Icon width={'21px'} height={'21px'} viewBox={'0 0 21 21'} iconId={'vk'}/>
-                    </SocialIconLink>
-                </SocialItems>
+            <S.SocialIconList>
 
-                <SocialItems>
-                    <SocialIconLink>
-                        <Icon width={'21px'} height={'21px'} viewBox={'0 0 21 21'} iconId={'linkedIn'}/>
-                    </SocialIconLink>
-                </SocialItems>
+                {socialItemData.map((item, index) => {
+                    return (
+                        <S.SocialItems>
+                            <S.SocialIconLink>
+                                <Icon width={'21px'} height={'21px'} viewBox={'0 0 21 21'} iconId={item.iconId}/>
+                            </S.SocialIconLink>
+                        </S.SocialItems>
+                    )
+                })}
 
-                <SocialItems>
-                    <SocialIconLink>
-                        <Icon width={'21px'} height={'21px'} viewBox={'0 0 21 21'} iconId={'telegram'}/>
-                    </SocialIconLink>
-                </SocialItems>
+            </S.SocialIconList>
 
-            </SocialIconList>
-            <Copyright>© 2023 Nikolay Litvinov, All Rights Reserved.</Copyright>
 
-        </FooterStyled>
+            <S.Copyright>© 2023 Nikolay Litvinov, All Rights Reserved.</S.Copyright>
+
+        </S.FooterStyled>
     );
 };
-
-const FooterStyled = styled.section`
-    padding-top: 40px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-
-
-`
-
-const Name = styled.span`
-${font({
-    family:'Josefin Sans, sans-serif',
-    weight:700,
-    Fmin:16,
-    Fmax:22
-    
-})}
-line-height: 22px;
-    letter-spacing: 3px;
-`
-
-const SocialIconList = styled.ul`
-    list-style: none;
-    display: flex;
-    padding: 0;
-    margin: 26px 30px 30px;
-    
-
-
-`
-
-const SocialIconLink = styled.a`
-    width: 35px;
-    height: 35px;
-    border-radius: 50%;
-    background-color: rgba(255,255,255,0.1);
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    color:${theme.colors.accent}
-    
-
-
-`
-const SocialItems = styled.li`    
-
-    &:nth-child(n+1){
-        margin-left: 20px;
-    } 
-    
-    &:hover {
-    ${SocialIconLink} {
-    background-color: ${theme.colors.accent};
-        transform: translateY(-5px);
-        color: black;
-    }
-        
-    
-}
-     
-    
-
-
-`
-
-const Copyright = styled.small`
-
-    color: rgba(255, 255, 255, 0.5);
-    font-family: Poppins, sans-serif;
-    font-size: 12px;
-    font-weight: 400;
-    line-height: 18px;    
-    text-align: center;
-
-`
 
---
+++ src/layout/footer/FooterStyles.ts
@@ -0,0 +1,91 @@
+import styled from "styled-components";
+import {font} from "../../styles/Common";
+import {theme} from "../../styles/Theme";
+
+const FooterStyled = styled.section`
+    padding-top: 40px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+
+`
+
+const Name = styled.span`
+${font({
+    family:'Josefin Sans, sans-serif',
+    weight:700,
+    Fmin:16,
+    Fmax:22
+
+})}
+line-height: 22px;
+    letter-spacing: 3px;
+`
+
+const SocialIconList = styled.ul`
+    list-style: none;
+    display: flex;
+    padding: 0;
+    margin: 26px 30px 30px;
+    
+
+
+`
+
+const SocialIconLink = styled.a`
+    width: 35px;
+    height: 35px;
+    border-radius: 50%;
+    background-color: rgba(255,255,255,0.1);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color:${theme.colors.accent}
+    
+
+
+`
+const SocialItems = styled.li`    
+
+    &:nth-child(n+1){
+        margin-left: 20px;
+    } 
+    
+    &:hover {
+    ${SocialIconLink} {
+    background-color: ${theme.colors.accent};
+        transform: translateY(-5px);
+        color: black;
+    }
+        
+    
+}
+     
+    
+
+
+`
+
+const Copyright = styled.small`
+
+    color: rgba(255, 255, 255, 0.5);
+    font-family: Poppins, sans-serif;
+    font-size: 12px;
+    font-weight: 400;
+    line-height: 18px;    
+    text-align: center;
+
+`
+
+
+
+
+export const S = {
+     FooterStyled,
+     Name,
+     SocialIconList,
+     SocialIconLink,
+     SocialItems,
+     Copyright
+ }
